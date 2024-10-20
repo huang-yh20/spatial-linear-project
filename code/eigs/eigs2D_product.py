df18aeeafd9ad96e5fb36bfd3a87a8f25f784ef4
huang-yh20 <2446075015@qq.com>
2024-10-20 15:43:06 +0900

    debug on eigs2D

--- code/eigs/eigs2D_product.py
+++ code/eigs/eigs2D_product.py
@@ -33,14 +33,15 @@ file_name_n, param_n = int(args[0]), int(args[1])
 for repeat_trial in range(repeat_num):
     if os.path.exists(r'./data/' +file_name_old_list[file_name_n] + str(repeat_trial) + str(param_n) + 'eig.npy'):
         eigs = np.load(r'./data/' +file_name_old_list[file_name_n] + str(repeat_trial) + str(param_n) + 'eig.npy')
-        np.save(r'./data/eigs_' +file_name_new_list[param_n] + '_' + str(param_n) + '_' + str(repeat_trial) + 'eig.npy', eigs)
+        np.save(r'./data/eigs_' +file_name_new_list[file_name_n] + '_' + str(param_n) + '_' + str(repeat_trial) + 'eig.npy', eigs)
         os.remove(r'./data/' +file_name_old_list[file_name_n] + str(repeat_trial) + str(param_n) + 'eig.npy')
     else:
-        generate_eigs_pramas = generate_func_list[param_n]
-        p_net = generate_eigs_pramas(param_n)
+        if not os.path.exists(r'./data/eigs_' +file_name_new_list[file_name_n] + '_' + str(param_n) + '_' + str(repeat_trial) + 'eig.npy'):
+            generate_eigs_pramas = generate_func_list[param_n]
+            p_net = generate_eigs_pramas(param_n)
 
-        dist_list = calc_dist(p_net, dim = 2)
-        J = generate_net(p_net, dist_list)
-        eigs, eig_V = np.linalg.eig(J)
-        np.save(r'./data/eigs_' +file_name_new_list[param_n] + '_' + str(param_n) + '_' + str(repeat_trial) + 'eig.npy', eigs)
-        
+            dist_list = calc_dist(p_net, dim = 2)
+            J = generate_net(p_net, dist_list)
+            eigs, eig_V = np.linalg.eig(J)
+            np.save(r'./data/eigs_' +file_name_new_list[file_name_n] + '_' + str(param_n) + '_' + str(repeat_trial) + 'eig.npy', eigs)
+            
