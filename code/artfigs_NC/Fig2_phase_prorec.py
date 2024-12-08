c6986d4a6a13e75426fdfac25c900c57cd04a5bf
huang-yh20 <2446075015@qq.com>
2024-12-08 16:32:03 +0900

    new phase of tanh

--- code/artfigs_NC/Fig2_phase_prorec.py
+++ code/artfigs_NC/Fig2_phase_prorec.py
@@ -6,7 +6,7 @@ from spatial_ultis import *
 from dyn_ultis import *
 from artfigs_NC_params import *
 
-repeat_num = 1
+repeat_num = 5
 trial_num = 21
 
 if __name__ == "__main__":
@@ -14,10 +14,10 @@ if __name__ == "__main__":
 trial1, trial2 = int(args[0]), int(args[1])
 
 for repeat_trial in range(0,repeat_num):
-    p_simul = Simul_Params(T = 2000, t_step=5, record_step=10, activation_func=['thres_linear','thres_powerlaw'], external_input="DC_noise",tau_m=20.0)
-    p_net = generate_params_phase_d_II_g_bar_II_thres_L(trial1, trial2, trial_num)
-    record_x = dyn_simul(p_net, p_simul, dim=2, homo_fix_point=True)
-    np.save(r"./data/artfigs_NC_"+'homo_d_II_g_bar_II_thres_L'+'_'+str(trial1)+'_'+str(trial2)+'_'+str(repeat_trial)+r'.npy',record_x)
+    # p_simul = Simul_Params(T = 2000, t_step=5, record_step=10, activation_func=['thres_linear','thres_powerlaw'], external_input="DC_noise",tau_m=20.0)
+    # p_net = generate_params_phase_d_II_g_bar_II_thres_L(trial1, trial2, trial_num)
+    # record_x = dyn_simul(p_net, p_simul, dim=2, homo_fix_point=True)
+    # np.save(r"./data/artfigs_NC_"+'homo_d_II_g_bar_II_thres_L'+'_'+str(trial1)+'_'+str(trial2)+'_'+str(repeat_trial)+r'.npy',record_x)
 
     # p_simul = Simul_Params(T = 2000, t_step=5, record_step=10, activation_func=['tanh','tanh_high'], external_input="noise",tau_m=20.0)
     # p_net = generate_params_phase_d_II_g_bar_II_L(trial1, trial2, trial_num)
@@ -29,3 +29,10 @@ for repeat_trial in range(0,repeat_num):
     # p_net = generate_params_phase_g_d_II_L_chaos(trial1, trial2, trial_num)
     # record_x = dyn_simul(p_net, p_simul, dim=2, homo_fix_point=False)
     # np.save(r"./data/artfigs_NC_"+'g_d_II_L_chaos'+'_'+str(trial1)+'_'+str(trial2)+'_'+str(repeat_trial)+r'.npy',record_x)    
+
+
+    p_simul = Simul_Params(T = 2000, t_step=5, record_step=10, activation_func=['tanh','tanh_high'], external_input="noise",tau_m=20.0)
+    p_net = generate_params_phase_d_II_g_bar_II_S(trial1, trial2, trial_num)
+    record_x = dyn_simul(p_net, p_simul, dim=2, homo_fix_point=False)
+    np.save(r"./data/artfigs_NC_"+'d_II_g_bar_II_S'+'_'+str(trial1)+'_'+str(trial2)+'_'+str(repeat_trial)+r'.npy',record_x)    
+
